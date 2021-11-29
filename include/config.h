4ce651116cc9d45d2d4b6dab4aed4c55b3f5f8d2
Paranoramix <gilles.leonis@gmail.com>
2021-11-29 23:26:15 +0100

    mise à jour des variabels

--- include/config.h
+++ include/config.h
@@ -15,24 +15,36 @@
 // Variables for GSM
 #define GsmSerial Serial2
 
-const char* simPin       = "0000";
+const char* simPin        = "0000";
 
-const char gsmApn[]      = "orange.fr";
-const char gsmGprsUser[] = "orange";
-const char gsmGprsPass[] = "orange";
+const char gsmApn[]       = "orange.fr";
+const char gsmGprsUser[]  = "orange";
+const char gsmGprsPass[]  = "orange";
+
+String modemName          = "";
+String modemInfo          = "";
+String simStatus          = "";
+String gprsStatus         = "";
+String ccid               = "";
+String imei               = "";
+String imsi               = "";
+String gsmOperator        = "";
+String signalQuality      = "";
+String networkMode        = "";
+String registrationStatus = "";
 
 /////////////////////////////////////////////////////////////////////
 // Variables for GPS
-  float latitude         = 0;
-  float longitude        = 0;
-  float speed            = 0;
-  float altitude         = 0;
-  int   visibleSat       = 0;
-  int   usedSat          = 0;
-  float accuracy         = 0;
-  int   year             = 0;
-  int   month            = 0;
-  int   day              = 0;
-  int   hour             = 0;
-  int   minute           = 0;
-  int   second           = 0;
+  float latitude          = 0;
+  float longitude         = 0;
+  float speed             = 0;
+  float altitude          = 0;
+  int   visibleSat        = 0;
+  int   usedSat           = 0;
+  float accuracy          = 0;
+  int   year              = 0;
+  int   month             = 0;
+  int   day               = 0;
+  int   hour              = 0;
+  int   minute            = 0;
+  int   second            = 0;
